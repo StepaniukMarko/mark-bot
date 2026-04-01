a9f011fa67a9d73c5e17da6ec7b76e5f0a87ebe6
StepaniukMarko <stepaniukmarko@gmail.com>
2026-04-01 13:33:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ from telegram.ext import (
 #  КОНФІГ
 # ══════════════════════════════════════
 TELEGRAM_TOKEN = "8542977906:AAHAWLOUpzhawn6UCDytCdhH8Xdqh4XnVkE"
-GROQ_API_KEY   = "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"
+GROQ_API_KEY   = "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"
 GROQ_URL       = "https://api.groq.com/openai/v1/chat/completions"
 DIALOG_FILE    = "dialog_history.json"
 NOTES_FILE     = "notes_tg.txt"
